fd5a2f53d50b4282c131f14684a12167108fa585
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-23 18:19:44 +0200

    move dbContext configuration to extension method

---
+++ PhoneBook/PhoneBook.Repositories/Extensions/PhoneBookDbExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PhoneBook.Repositories.Data;
+
+namespace PhoneBook.Repositories.Extensions
+{
+    public static class PhoneBookDbExtensions
+    {
+        public static void AddPhoneBookDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<PhoneBookDbContext>(options => options
+              .UseSqlServer(config.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
--- PhoneBook/PhoneBook/Startup.cs
+++ PhoneBook/PhoneBook/Startup.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using PhoneBook.Repositories.Data;
+using PhoneBook.Repositories.Extensions;
 
 namespace PhoneBook
 {
@@ -25,8 +19,8 @@ namespace PhoneBook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<PhoneBookDbContext>(options => options
-               .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddPhoneBookDbContext(Configuration);
+            services.AddRepositoryWrapper();
 
             services.AddMvc();
         }
