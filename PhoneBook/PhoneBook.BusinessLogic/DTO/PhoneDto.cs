d4343357adb886f369221e6be40b2aed5c31f792
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-24 00:43:56 +0200

    add DTOs to business logic layer

---
+++ PhoneBook/PhoneBook.BusinessLogic/DTO/PhoneDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PhoneBook.BusinessLogic.DTO
+{
+    public class PhoneDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public UserDto User { get; set; }
+    }
+}
---
+++ PhoneBook/PhoneBook.BusinessLogic/DTO/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhoneBook.BusinessLogic.DTO
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public bool IsDeleted { get; set; }
+        public IEnumerable<PhoneDto> Phones { get; set; }
+    }
+}
