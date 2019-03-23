fe8936406604774ba10793c634be168f0d0ff843
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-23 18:16:46 +0200

    implement user and phone repository

---
+++ PhoneBook/PhoneBook.Repositories/Data/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using PhoneBook.Contracts;
+using PhoneBook.Domains;
+
+namespace PhoneBook.Repositories.Data
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(PhoneBookDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
