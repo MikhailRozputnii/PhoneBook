f80945c316b34f998ee6919ef63f739ae8adc41b
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-23 18:18:38 +0200

    implement unit of work pattern and add configuration

---
+++ PhoneBook/PhoneBook.Contracts/IRepositoryWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PhoneBook.Contracts
+{
+    public interface IRepositoryWrapper: IDisposable
+    {
+        IUserRepository User { get; }
+        IPhoneRepository Phone { get; }
+        void Save();
+    }
+}
