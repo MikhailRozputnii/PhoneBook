79191787c768b25a8f635dc69cdc59eff5c775eb
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-24 00:36:34 +0200

    change domains

--- PhoneBook/PhoneBook.Domains/Phone.cs
+++ PhoneBook/PhoneBook.Domains/Phone.cs
@@ -4,7 +4,7 @@ namespace PhoneBook.Domains
 {
     public class Phone
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
         public string Description { get; set; }
--- PhoneBook/PhoneBook.Domains/User.cs
+++ PhoneBook/PhoneBook.Domains/User.cs
@@ -5,7 +5,7 @@ namespace PhoneBook.Domains
 {
     public class User 
     {
-        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid Id { get; set; } 
         public string Email { get; set; }
         public string PasswordHash { get; set; }
         public bool IsDeleted { get; set; }
