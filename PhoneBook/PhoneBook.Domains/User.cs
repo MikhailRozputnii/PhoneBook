e58da83818f52d534488d5054a552aab3a973b24
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-23 13:28:33 +0200

    implement domain models

---
+++ PhoneBook/PhoneBook.Domains/User.cs
@@ -0,0 +1,17 @@
+﻿using PhoneBook.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.Domains
+{
+    public class User : IEntity
+    {
+        public Guid Id { get; set; }
+        public string Email {get;set;}
+        public string PasswordHash { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public IEnumerable<Phone> Phones { get; set; }
+    }
+}
