8c1f2086d4a139c0ede831ff1be725ff1a17c22b
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-24 00:42:16 +0200

    add AutoMapper and configure mapper profile

---
+++ PhoneBook/PhoneBook.BusinessLogic/Configs/MapperProfiles/DomainProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PhoneBook.BusinessLogic.DTO;
+using PhoneBook.Domains;
+
+namespace PhoneBook.BusinessLogic.Configs.MapperProfiles
+{
+    public class DomainProfile : Profile
+    {
+        public DomainProfile()
+        {
+            CreateMap<User, UserDto>();
+        }
+    }
+}
