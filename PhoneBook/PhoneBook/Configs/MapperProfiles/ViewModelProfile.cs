99d60670b32f7fd61daf8b9bb3f78b64427d7078
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-25 00:12:15 +0200

    add mapper profile

---
+++ PhoneBook/PhoneBook/Configs/MapperProfiles/ViewModelProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PhoneBook.BusinessLogic.DTO;
+using PhoneBook.Domains;
+using PhoneBook.Models;
+
+namespace PhoneBook.Configs.MapperProfiles
+{
+    public class ViewModelProfile : Profile
+    {
+        public ViewModelProfile()
+        {
+            CreateMap<PhoneDto, PhoneViewModel>();
+        }
+    }
+}
