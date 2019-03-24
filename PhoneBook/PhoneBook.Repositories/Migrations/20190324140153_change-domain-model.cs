f2be5148bf5f724b87a8111145ffd6994fbd0949
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-24 16:05:17 +0200

    update migrations

---
+++ PhoneBook/PhoneBook.Repositories/Migrations/20190324140153_change-domain-model.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PhoneBook.Repositories.Migrations
+{
+    public partial class changedomainmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedEmail",
+                table: "Users",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NormalizedEmail",
+                table: "Users");
+        }
+    }
+}
