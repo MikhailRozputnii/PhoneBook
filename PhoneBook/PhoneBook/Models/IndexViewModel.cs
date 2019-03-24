4e073171d720c6b1af8f9994c2f1a7614757890d
Mikhail Rozputnii <mikhail.rozputnii@gmail.com>
2019-03-25 00:12:01 +0200

    add view models for get phone ui

---
+++ PhoneBook/PhoneBook/Models/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Models
+{
+    public class IndexViewModel
+    {
+        public int PageSize = 5;
+        public int CurrentPage { get; set; }
+        public string Search { get; set; }
+        public IEnumerable<PhoneViewModel> Phones { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+    }
+}
